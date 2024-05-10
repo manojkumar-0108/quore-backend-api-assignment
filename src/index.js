3143e133f65c868e86750def78de2a970aeb0fd0
Manoj Kumar <manoj8894421801@gmail.com>
2024-05-10 14:36:57 +0530

    FEAT: added rate limiter and body parser middlewares:

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 
 const { serverConfig } = require('./config');
 const { pingCheck } = require('./controllers');
 
-
-
 const app = express();
 
+// Rate Limiter
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).  
+})
+app.use(limiter);
+
+
+//body parser middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.text());
+app.use(express.raw());
+
+/**
+ * GET: http://localhost:4000/ping
+ */
 app.get('/ping', pingCheck('Server is live...'));
 
+
+// error handler
 app.listen(serverConfig.PORT, () => {
     console.log(`Started server at PORT:${serverConfig.PORT}`);
 })
