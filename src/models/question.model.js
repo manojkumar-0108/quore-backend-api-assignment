3f0d9b89c0e9d304166ad778c5665e8f643a197e
Manoj Kumar <manoj8894421801@gmail.com>
2024-05-11 11:21:12 +0530

    FEAT: Question & Topic schema added with M:N relationship

---
+++ src/models/question.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const questionSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'Title of the question cannot be null']
+    },
+    body: {
+        type: String,
+        required: [true, 'body of the question cannot be null']
+    },
+    topics: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Topic'
+        }
+    ], // Reference to Topic model
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    } // Reference to User model
+});
+
+const Question = model('Question', questionSchema);
+module.exports = Question;
